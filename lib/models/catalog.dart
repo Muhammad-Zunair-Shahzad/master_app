a2b7de4ca219a27fbf536518469335226886a63f
Muhammad-Zunair-Shahzad <zunairshahzad2004@gmail.com>
2024-09-24 11:55:03 +0500

    Day12-Part2 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,15 +1,63 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+// When you declare CatalogModel.items as nullable (like List<Item>?), the compiler assumes that items could be null...
+// Therefore, when you try to access its properties like (itemCount: CatalogModel.items!.length), the compiler gives an error because it cannot guarantee 
+// that items has been initialized with a non-null value...
+// When you use CatalogModel.items!.length, you're forcing the compiler to treat items as non-null.
+// If items is null at runtime and you use the ! operator, it will result in a runtime error (Null check operator used on a null value).
+class CatalogModel {
+  static List<Item>? items;
+}
+
 class Item {
+  final int id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
   Item(
       {required this.id,
       required this.name,
       required this.desc,
       required this.price,
       required this.color,
-      required this.image}
-      );
+      required this.image});
 
-  // "Decode" OR simply access values as map["id"]
+  Item copyWith({
+    int? id,
+    String? name,
+    String? desc,
+    num? price,
+    String? color,
+    String? image,
+  }) {
+    return Item(
+      id: this.id,
+      name: this.name,
+      desc: this.desc,
+      price: this.price,
+      color: this.color,
+      image: this.image,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    // I remove ??
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'desc': desc,
+      'price': price,
+      'color': color,
+      'image': image,
+    };
+  }
+
   factory Item.fromMap(Map<String, dynamic> map) {
+    // I remove as
     return Item(
       id: map["id"],
       name: map["name"],
@@ -20,46 +68,35 @@ class Item {
     );
   }
 
-  //encode 1
-  toMap() => {
-        "id": id,
-        "name": name,
-        "desc": desc,
-        "price": price,
-        "color": color,
-        "image": image,
-      };
+  String toJson() => json.encode(toMap());
 
-// encode 1
-  //  Map<String,dynamic> get toMap{
-  //   return {
-  //       "id": id,
-  //       "name": name,
-  //       "desc": desc,
-  //       "price": price,
-  //       "color": color,
-  //       "image": image,
-  //     };
-  //  }
+  factory Item.fromJson(String source) =>
+      Item.fromMap(json.decode(source) as Map<String, dynamic>);
 
-  final int id;
-  final String name;
-  final String desc;
-  final num price;
-  final String color;
-  final String image;
-}
+  @override
+  String toString() {
+    return 'Item(id: $id, name: $name, desc: $desc, price: $price, color: $color, image: $image)';
+  }
 
-class CatalogModel {
-  static List<Item> items = [
-    // items stores the instances list, of the Item class
-    Item(
-        id: 1,
-        name: "iPhone 12 Pro",
-        desc: "Apple iPhone 12th generation",
-        price: 999,
-        color: "#33505a",
-        image:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc"),
-  ];
+  @override
+  bool operator ==(covariant Item other) {
+    if (identical(this, other)) return true;
+
+    return other.id == id &&
+        other.name == name &&
+        other.desc == desc &&
+        other.price == price &&
+        other.color == color &&
+        other.image == image;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        name.hashCode ^
+        desc.hashCode ^
+        price.hashCode ^
+        color.hashCode ^
+        image.hashCode;
+  }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,76 +7,73 @@ import 'package:master_app/widgets/item_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
-
   @override
   State<HomePage> createState() => _HomePageState();
 }
-
 class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
     loadData();
   }
-
+// CatalogJason stores the JSON file into string
+// DecodeData stores the Decoded String
+// ProductData stores the value of products key, and that value is a List of Maps starting from [0] index to [7] index
+// Now we cannot use List of Maps so covert it into a simple List
+// - List.from(productData) -> Which list of Map you want to convert ? A) productData
+// - map<Item>((item)) -> join the (individual map or objects or instances) 1 by 1 with the named constructor (fromMap(item)) of Item class
+// - Firstly [0] index Map or obj or instance goes to named constructor
+// - Secondly [1] index and so on upto Map at [7] index ...
+// - At the end we created a List out of List of Map and also assign each Map to Item class
+// - Note that what is the type of that List ? A) List<Item> . Which is same as Type of CatalogModel.items so replace 'List<Item> list' with 'CatalogModel.items'
+// After the evaluation of R.H.S, we simply get a List of Instances of Item Class instead of List of Maps which is like
+// - [Instance of 'Item', Instance of 'Item', Instance of 'Item', Instance of 'Item', Instance of 'Item', Instance of 'Item', Instance of 'Item', Instance of 'Item']
   loadData() async {
-    final catalogJson =
-        await rootBundle.loadString("assets/files/catalog.json");
+    await Future.delayed(const Duration(seconds: 5)); // Wait for 5 sec in order to see this error: "Null check operator used on a null value..."
+    final catalogJson = await rootBundle.loadString("assets/files/catalog.json");
     final decodedData = jsonDecode(catalogJson);
     final productData = decodedData["products"];
-
-    // As List<Item>list is same as CatalogModel.items so
-    CatalogModel.items =
-        List.from(productData).map<Item>((item) => Item.fromMap(item)).toList();
-
-    debugPrint(CatalogModel.items.toString());
-
-    // List<Item> list = List.from(productData)
-    // .map<Item>((item) => Item.fromMap(item))
-    // .toList();
-
-    // Above is same as
-    // static final List<Item> items = [ // This is a list we made above
-    //   Item(
-    //       id: 1,
-    //       name: "iPhone 12 Pro",
-    //       desc: "Apple iPhone 12th generation",
-    //       price: 999,
-    //       color: "#33505a",
-    //       image:
-    //           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc"),
-    //];
+    CatalogModel.items =List.from(productData).map<Item>((item) => Item.fromMap(item)).toList();
+    setState(() {});
   }
-
-// This is given by chatgpt 
-//   loadData() async {
-//   final catalogJson = await rootBundle.loadString("assets/files/catalog.json");
-//   var decodedData = jsonDecode(catalogJson);
-//   var productsData = decodedData['products'];
-//   for (var product in productsData) {
-//     print("Product name: ${product['name']}");
-//   }
-// }
-
   @override
   Widget build(BuildContext context) {
-    //final dummyList = List.generate(5, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Catalog App'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: CatalogModel.items.length,
+        child:(CatalogModel.items != null && CatalogModel.items!.isNotEmpty)? ListView.builder(
+          itemCount: CatalogModel.items!.length, 
           itemBuilder: (context, index) {
             return ItemWidget(
-              item: CatalogModel.items[index],
+              item: CatalogModel.items![index],
             );
           },
+        ): const Center(
+          child: CircularProgressIndicator(),
         ),
       ),
       drawer: const MyDrawer(),
     );
   }
 }
+
+/*
+ - Although we force that length cannot be null, but loadData takes some time to convert Json to string, 
+   and then decode etc, during this time dart compiler consider the length will be null, and gives an error 
+   later on when compiler comes to this line where we forcefuly say that length is not null, then after 
+   sometime images and all other data display on the screen
+
+ - CatalogModel.items != null means the variable itself must have been assigned a valid list reference
+
+ - CatalogModel.items!.isNotEmpty  (This si enough instead of writing the CatalogModel.items != null but comprisable)
+      * It asserts that items is not null
+      * It checks that items is not empty
+
+ - The ! operator asserts that CatalogModel.items is not null. 
+   If items is null, it will throw a runtime error. So, using ! 
+   guarantees that you believe items is initialized (not null).
+
+*/
